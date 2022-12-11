b45f533feb8736f872bb620a3406be778a9f4990
Andrii Murlykin <109280143+Murlykin@users.noreply.github.com>
2022-12-11 11:55:52 +0200

    message

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+const formData = {};
+
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  textarea: document.querySelector('.feedback-form textarea'),
+  email: document.querySelector('.feedback-form input'),
+};
+
+populatedForm();
+
+refs.form.addEventListener('input', throttle(onFormInput, 500));
+refs.form.addEventListener('submit', onFormSubmit);
+
+function onFormInput(e) {
+  const formName = e.target.name;
+  const formValue = e.target.value;
+  formData[formName] = formValue;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function populatedForm() {
+  const savedFormData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (savedFormData) {
+    refs.email.value = savedFormData.email;
+    refs.textarea.value = savedFormData.message;
+  }
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  e.currentTarget.reset();
+  console.log(localStorage.getItem(STORAGE_KEY));
+  localStorage.removeItem(STORAGE_KEY);
+}
